13bfe8f36274697b1d25c0efd462a426f849e5ea
Guilherme <gqgs@protonmail.com>
2020-12-13 00:18:35 -0300

    feat: parallel searches

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 type options struct {
-	output string
-	input  string
+	output   string
+	input    string
+	parallel int
 }
 
 func main() {
@@ -23,6 +26,7 @@ func main() {
 	var o options
 	flag.StringVar(&o.input, "input", "", "input folder")
 	flag.StringVar(&o.output, "output", wd, "output folder")
+	flag.IntVar(&o.parallel, "parallel", runtime.NumCPU(), "number of parallel processes")
 	flag.Parse()
 
 	if err := process(o); err != nil {
@@ -40,7 +44,7 @@ func process(o options) error {
 		return err
 	}
 
-	return filepath.Walk(o.input, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(o.input, func(path string, info os.FileInfo, err error) error {
 		relativePath := strings.TrimPrefix(path, o.input)
 		relativePath = strings.TrimLeft(relativePath, "/")
 		if relativePath == "" {
@@ -52,25 +56,70 @@ func process(o options) error {
 			return os.Mkdir(newFolder, os.ModePerm)
 		}
 
-		if isConvertible(relativePath) {
-			return convert(relativePath, o.input, outFolder)
-		}
-
-		inFile, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer inFile.Close()
-
-		outFile, err := os.Create(filepath.Join(outFolder, relativePath))
-		if err != nil {
-			return err
-		}
-		defer outFile.Close()
-
-		_, err = io.Copy(outFile, inFile)
-		return err
+		return nil
 	})
+
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	pathCh := make(chan string)
+
+	go func() {
+		semaphore := make(chan struct{}, o.parallel)
+		for path := range pathCh {
+			path := path
+			semaphore <- struct{}{}
+			go func() {
+				defer func() {
+					<-semaphore
+					wg.Done()
+				}()
+
+				if err := func() error {
+					relativePath := strings.TrimPrefix(path, o.input)
+					relativePath = strings.TrimLeft(relativePath, "/")
+
+					if isConvertible(relativePath) {
+						return convert(relativePath, o.input, outFolder)
+					}
+
+					inFile, err := os.Open(path)
+					if err != nil {
+						return err
+					}
+					defer inFile.Close()
+
+					outFile, err := os.Create(filepath.Join(outFolder, relativePath))
+					if err != nil {
+						return err
+					}
+					defer outFile.Close()
+
+					_, err = io.Copy(outFile, inFile)
+					return err
+				}(); err != nil {
+					log.Printf("%s: %s", err, path)
+				}
+
+			}()
+		}
+	}()
+
+	err = filepath.Walk(o.input, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		wg.Add(1)
+		pathCh <- path
+
+		return nil
+	})
+	wg.Wait()
+
+	return err
 }
 
 func isConvertible(path string) bool {
@@ -81,7 +130,8 @@ func convert(relativePath, baseFolder, outFolder string) error {
 	mp3FileName := strings.TrimSuffix(relativePath, ".flac") + ".mp3"
 	newPath := filepath.Join(outFolder, mp3FileName)
 	originalPath := filepath.Join(baseFolder, relativePath)
-	cmd := exec.Command("ffmpeg", "-i", originalPath, "-q:a", "0", "-threads", "0", newPath)
+	log.Printf("Creating: %q", mp3FileName)
+	cmd := exec.Command("ffmpeg", "-i", originalPath, "-q:a", "0", "-threads", "0", "-loglevel", "error", newPath)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
